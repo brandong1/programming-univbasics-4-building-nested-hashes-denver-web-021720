fd9f3c9b5f6a97cc3f474d4bc635c3fdbd8cbb54
Brandon Green <brandon@brandonleeproductions.com>
2020-01-10 03:23:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,9 @@ def monopoly_with_third_tier
       pieces: 4,
       names: {},
       rent_in_dollars: {
-        :one_piece_owned => 25 
-        :two_pieces_owned => 25 
-        :three_piece_owned => 25 
+        :one_piece_owned => 25,
+        :two_pieces_owned => 25,
+        :three_piece_owned => 25, 
         :four_piece_owned => 25 
       }
     }
