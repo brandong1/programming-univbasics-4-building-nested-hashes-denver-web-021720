500c9eda087ceb3339027ca343065daa06e29dc7
Brandon Green <brandon@brandonleeproductions.com>
2020-01-10 03:45:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,9 +37,7 @@ def monopoly_with_fourth_tier
     railroads: {
       pieces: 4,
       names: {
-        :reading_railroad => {
-          mortgage_value: 
-        },
+        :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
