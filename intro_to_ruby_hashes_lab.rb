b2ee72058c4964c847183a21cfd85dba71311d23
Brandon Green <brandon@brandonleeproductions.com>
2020-01-10 02:59:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,8 +4,10 @@ end
 
 def monopoly_with_second_tier
   monopoly = {
-    :railroads => {
-      :pieces
+    railroads: {
+      pieces: 4,
+      names: {},
+      rent_in_dollars: {}
     }
   }
 end
